2767e6f6ecc22ff202afd98c3d46543a239da9b1
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-03-28 20:30:53 -0400

    added js code for array with objects for the question banks and answers. Also added query selectors for each of the HTML IDs and classes.

---
+++ script.js
@@ -0,0 +1,48 @@
+var mainEl = document.getElementById("main");
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+var question = document.getElementById("question")
+const choiceA = document.getElementById("A")
+const choiceB = document.getElementById("B")
+const choiceC = document.getElementById("C")
+const choiceD = document.getElementById("D")
+// stick with var here instead of const
+// populate the HTML with the above IDs
+var questions = [
+  {
+      questionStem: "What does HTML stand for?",
+      choiceA: "Hot Text Markup Language",
+      choiceB: "Hyper Text Markup Language",
+      choiceC: "High Text Markup Language",
+      choiceD: "Hidden Text Markup Language",
+      correct: "B"
+  },
+  {
+      questionStem: "What does JS stand for?",
+      choiceA: "Javasuper",
+      choiceB: "Javasingle",
+      choiceC: "Jabroni's suck",
+      choiceD: "Javascript",
+      correct: "D"
+  },
+];
+var lastQuestionIndex = questions.length - 1;
+var questionBank = 0;
+function showQuestion(){
+  var q = questions[questionBank];
+  question.innerHTML = "<p>" + q.question + "</p>";
+  choiceA.innerHTML = q.choiceA;
+  choiceB.innerHTML = q.choiceB;
+  choiceC.innerHTML = q.choiceC;
+  choiceD.innerHTML = q.choiceD;
+  console.log("The question is shown");
+}
+ 
+function setTimer() {
+ 
+}
+ 
+ 
+showQuestion();
+ 
+ 
