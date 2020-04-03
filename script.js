49b88943543c4e738331395c2cdc3c0cec2ecf56
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-04-03 08:56:35 -0400

    cleared up useless space on js document.

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ var startQuiz = document.getElementById("startQuizButton");
 var quizScore = 0;
 var count = 0;
 var screenScore = document.querySelector("#screenScore");
-
+var lastQuestionIndex = questions.length - 1;
+var questionBank = 0;
 var questions = [
   {
     questionStem: "What is Elliott's favorite color?",
@@ -55,10 +56,6 @@ var questions = [
     correct: "D"
   },
 ];
-
-var lastQuestionIndex = questions.length - 1;
-var questionBank = 0;
-
 function showQuestion(){
   var q = questions[questionBank];
   question.innerHTML = "<p>" + q.questionStem + "</p>";
@@ -68,7 +65,6 @@ function showQuestion(){
   choiceD.innerHTML = q.choiceD;
   console.log("The question is shown");
 }
-
 function cycleQuestions(){
   questionBank++;
   if(questionBank > lastQuestionIndex){
@@ -77,7 +73,6 @@ function cycleQuestions(){
   showQuestion();
   }
 }
-
 function userStartQuiz(){
   console.log("the quiz has started.");
   setTime();
@@ -89,16 +84,6 @@ function userStartQuiz(){
   }
   showQuestion();
 }
-
-// var scores = JSON.parse(window.localStorage.setItem('scores'));
-//   if (scores === null) {
-//     scores = [];
-//   } 
-//   scores.push(currentPlayerScore);
-//   localStorage.setItem("scores", JSON.stringify(scores));
-//   viewHighScores(event);
-// }
-
 function setTime() {
   // Should I put an if statement in here so when the user clicks on the button it runs the timer?
   var timerInterval = setInterval(function() {
@@ -111,12 +96,10 @@ function setTime() {
 
   }, 1000);
 }
-
 function setCounterText() {
   screenScore.textContent = count;
 }
 var count = localStorage.getItem("screenScore");
- 
 // create addEventListener for onclick to work with the cycleQuestions,, when user selects an answer choice
 startQuiz.addEventListener("click", userStartQuiz);
 
